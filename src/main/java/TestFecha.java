bfd9c14c7d2eeb5afb9a759b36d777b443ff85cc
thomas <t.kinderman01@ufromail.cl>
2023-04-29 14:28:54 -0400

    commit

--- src/main/java/TestFecha.java
+++ src/main/java/TestFecha.java
@@ -1,2 +1,6 @@
 public class TestFecha {
+    public static void main(String[] args) {
+        TodaysDate hoy = new TodaysDate();
+        hoy.printDateAndTime();
+    }
 }
--- src/main/java/TodaysDate.java
+++ src/main/java/TodaysDate.java
@@ -1,2 +1,28 @@
+import  java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TodaysDate {
+
+    private String time;
+    public int day;
+    private int  month;
+    protected String year;
+
+    public void printDateAndTime(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        time = String.valueOf(calendar.get(Calendar.HOUR_OF_DAY)) + ":" +
+                String.valueOf(calendar.get(Calendar.MINUTE)) + ":" +
+                String.valueOf(calendar.get(Calendar.SECOND));
+
+        day = calendar.get(Calendar.DATE);
+        month = calendar.get(Calendar.MONTH) + 1;
+        year = String.valueOf(calendar.get(Calendar.YEAR));
+
+        System.out.println("Time: "+time);
+        System.out.println("Date: "+month+ " "+day + " " +year);
+
+    }
+
+
+
 }
